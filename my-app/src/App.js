4d8d2fc56cdc56e5010ebff4aeed7d55b90f9d76
Brionconroy <razvanbrion@gmail.com>
2023-11-29 11:33:35 +0000

    fisnhed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,6 +14,29 @@ function App() {
       <Content />
     </div>
   );
+}import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+// import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
+
+function App() {
+  return (
+    <div className={css.App}>
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
+    </div>
+  );
 }
+
+export default App;
 
 export default App;
